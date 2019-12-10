446ec8f3c7657790c28e196ff26be1090ed97df4
Jorge Rea <jorge.lrs497@gmail.com>
2019-12-10 01:05:40 -0600

    Cambiando registro

--- registro.php
+++ registro.php
@@ -6,43 +6,80 @@
 <body>
     <?php include "./assets/navbar.php" ?>
     <div class="container">
-        <img src="img/logo.png" class="img-fluid" width="100px">
-        <div class="card ">
-            <form method="#" action="#">
-                <fieldset>
-                    <legend class="alert-success">Crear cuenta</legend>
-                    <br>
-                    <label for="">Nombre De Usuario</label>
-                    <br>
-                    <input type="text" name="" placeholder="" size="30">
-                    <br>
-                    <label for="nombre">Nombres</label>
-                    <br>
-                    <input type="text" name="nombre" placeholder="" size="30">
-                    <br>
-                    <label for="">Apellido Paterno</label>
-                    <br>
-                    <input type="text" name="" placeholder="" size="30">
-                    <br>
-                    <label for="">Apellido Materno</label>
-                    <br>
-                    <input type="text" name="" placeholder="" size="30">
-                    <br>
-                    <label for="">Telefono</label>
-                    <br>
-                    <input type="text" name="" placeholder="" size="30">
-                    <br>
-                    <label for="Correo electrónico">Correo electrónico</label><br>
-                    <input type="text" name="Correo electrónico" placeholder="" size="30">
-                    <br>
-                    <label for="pass">Contraseña:</label>
-                    <br>
-                    <input type="password" name="pass" placeholder="Debe tener al menos 6 caracteres" size="30">
-                    <h6>La contraseña debe contener al menos seis caracteres.</h6>
-                    <br>
-                </fieldset>
+        <img src="img/logo.png" class="img-fluid p-2" width="100px">
+        <form class="card p-3">
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Nombre</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="nom" placeholder="Nombre">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Apellido Paterno</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="app" placeholder="Apellido Paterno">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Apellido Materno</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="apm" placeholder="Apellido Materno">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Contraseña</label>
+                <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Contraseña">
+                </div>
+            </div>
+            <fieldset class="form-group">
+                <div class="row">
+                <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
+                <div class="col-sm-10">
+                    <div class="form-check">
+                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                    <label class="form-check-label" for="gridRadios1">
+                        First radio
+                    </label>
+                    </div>
+                    <div class="form-check">
+                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                    <label class="form-check-label" for="gridRadios2">
+                        Second radio
+                    </label>
+                    </div>
+                    <div class="form-check disabled">
+                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
+                    <label class="form-check-label" for="gridRadios3">
+                        Third disabled radio
+                    </label>
+                    </div>
+                </div>
+                </div>
+            </fieldset>
+            <div class="form-group row">
+                <div class="col-sm-2">Checkbox</div>
+                <div class="col-sm-10">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck1">
+                    <label class="form-check-label" for="gridCheck1">
+                    Example checkbox
+                    </label>
+                </div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Terminar Registro</button>
+                </div>
+            </div>
             </form>
-        </div>
     </div>
 
 
--- scripts/guardaCliente.php
+++ scripts/guardaCliente.php
@@ -1,48 +1,18 @@
-<!doctype html>
-<html lang="es">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+include 'database.php';
+$db = new Database();
+$db->conectarBD();
+extract($_POST);
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-
-    <title>Registro</title>
-  </head>
-  <body>
-
-    <div class="container">
-        
-    <?php
-        include 'database.php';
-        $db= new Database();
-        $db->conectarBD();
-        extract($_POST);
-
-        $cadena ="INSERT INTO cliente(nomUsuarioC,nombresC,apPatC,apMatC,telefonoC,correoC,contraseñaC,numVisitas)
+$cadena = "INSERT INTO cliente(nomUsuarioC,nombresC,apPatC,apMatC,telefonoC,correoC,contraseñaC,numVisitas)
         VALUES ('$nombreU','$nombre','$apPaterno','$apMaterno','$telefono','$correo',
         '$contraseña','')";
 
-        $db->ejecutaSQL($cadena);
+$db->ejecutaSQL($cadena);
 
-        echo"
-            <div class='alert alert-success'>
+echo "<div class='alert alert-success'>
             Cliente Registrado
             </div>
         ";
-        header("refresh:3;../html/index.html");
-
-    ?>
-
-
-    </div>
-    
-
-    <!-- Optional JavaScript -->
-    <script src="../js/jquery-3.4.1.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    
-    
-  </body>
-</html>
+header("refresh:3;../html/index.html");
+?>
