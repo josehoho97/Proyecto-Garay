d02dff8e7389c5df14f09f387181d3585cf52945
Jorge Rea <jorge.lrs497@gmail.com>
2019-12-10 00:06:21 -0600

    No se lo que hago aqui señor hoho

---
+++ assets/footer.php
@@ -0,0 +1,23 @@
+<footer class="">
+    <div class="container">
+        <div class="row">
+            <div class="col-4 ">
+                <h3>ACERCA DE</h3>
+                <a class="" href="AcercaDe.html">¿Quienes somos?</a>
+                <br>
+                <a class="" href="">Recompensas</a>
+            </div>
+            <div class="col-4">
+                <h3>CONTACTATE CON NOSOTROS</h3>
+                <a href="https://www.facebook.com/Morelosbarquito/" class="" target="blank"><img src="img/facebook-icon.png" alt="icono de facebook" class=""></a></a>
+            </div>
+            <div class="col-4">
+                <h3>PIDE DESDE UBER EATS</h3>
+                <a href="https://www.ubereats.com/es-MX/torreon/food-delivery/barquito-morelos/Yn9-o8C5TRyVX5Hnf-pfiA/" class="">
+                    <img src="img/uber.png" alt="Icono Uber Eats" width="15%">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="container"> <span class="float-right">© 2019 El Barquito Morelos</span></div>
+</footer>
