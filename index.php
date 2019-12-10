ad6adff15660954504c8cbe4317fa9c2934f06bf
Jorge Rea <jorge.lrs497@gmail.com>
2019-12-10 00:08:56 -0600

    partindosela al proyecto

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+<!-- head -->
 <?php include "./assets/head.php";?>
 <body>
+
+    <!-- Navbar -->
     <?php include "./assets/navbar.php";?>
     <div class="container">
         <img src="img/inicio2.jpg" class="img-fluid" width="90%">
     </div>
+
+    <!-- Footer -->
     <?php include "./assets/footer.php";?>
     <script src="vendor/Jquery/jquery-2.2.4.min.js"></script>
     <script src="vendor/Bootstrap/js/bootstrap.min.js"></script>
